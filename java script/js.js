d4b1156f424de1fcaf92977f04e279d84c9d9f5d
haiem04 <minhhaitrandang@gmail.com>
2021-12-12 16:34:31 +0700

    thêm địa chỉ cửa hàng

--- java script/js.js
+++ java script/js.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded' , function(){
+  
+//------------------------------ ---------JS cho phần sticky menu
     var nav = document.getElementById('menu');
     var main = document.getElementById('main');
     var navbar = nav.offsetTop;
@@ -13,4 +15,28 @@ document.addEventListener('DOMContentLoaded' , function(){
              nav.classList.remove("sticky");
          }
     }
+
+//------------------------------ ---------JS cho phần địa chỉ cửa hàng
+    let list = document.querySelectorAll('.list');
+    let itemBox = document.querySelectorAll('.itemBox');
+    
+        for (let k = 1 ; k < itemBox.length; k++ ){
+            itemBox[k].classList.add('hide');
+        }
+        for ( let i = 0 ; i < list.length ; i++){
+            list[i].addEventListener('click' , function(){
+                console.log('hello');
+                let dataPlace = list[i].getAttribute('data-place');
+               
+                for ( let j = 0 ; j <  itemBox.length ;  j++){
+                    itemBox[j].classList.remove('active');
+                    itemBox[j].classList.add('hide');
+                    
+                    if ( itemBox[j].getAttribute('data-item') == dataPlace ){
+                    itemBox[j].classList.remove('hide');
+                    itemBox[j].classList.add('active');
+                    }
+                }
+            })
+        }
 })
